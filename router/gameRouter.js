6cc7aa5570078d236df88bc82e4d0feffe3b56f1
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-05-22 16:57:55 -0400

    first draft completed hw

--- router/gameRouter.js
+++ router/gameRouter.js
@@ -21,4 +21,87 @@ router.get("/get-all-games", function (req, res) {
   res.json({ payload: games });
 });
 
+router.get("/get-game-by-id/:id", function (req, res) {
+  games.forEach((item) => {
+    if (item.id === req.params.id) {
+      res.json({ payload: item });
+    }
+  });
+  res.json({ message: "The game with the id does not exist, please check id" });
+});
+
+////////////////////////////////////////////////
+///////////////////Extra Credit/////////////////
+//////////////////////Below/////////////////////
+////////////////////////////////////////////////
+router.get("/get-game-by-name/:name", function (req, res) {
+  let foundGame = games.findIndex(function (games) {
+    return (
+      games.game.replaceAll(" ", "").toLowerCase() ===
+      req.params.name.toLowerCase()
+    );
+  });
+
+  if (foundGame === -1) {
+    res.json({ message: "The game does not exist, please check name" });
+  } else {
+    res.json({
+      payload: games[foundGame],
+    });
+  }
+});
+////////////////////////////////////////////////
+///////////////////Extra Credit/////////////////
+//////////////////////Above/////////////////////
+////////////////////////////////////////////////
+
+router.post("/create-new-game", function (req, res) {
+  let newGameObj = {
+    id: uuidv4(),
+    game: req.body.game,
+    description: req.body.description,
+  };
+  let gameIndex = games.findIndex((games) => games.game === newGameObj.game);
+
+  if (req.body.game === undefined || req.body.description === undefined) {
+    res.json({ message: "cannot leave text area blank" });
+  } else if (gameIndex > -1) {
+    res.json({ message: "Game already exists, cannot add game" });
+  }
+
+  games.push(newGameObj);
+  res.json({ payload: games });
+});
+
+router.put("/update-game/:id", function (req, res) {
+  let gameIndex = games.findIndex((games) => games.id === req.params.id);
+  if (gameIndex === -1) {
+    res.json({ message: "game not found, cannot update" });
+  } else {
+    if (req.body.description === undefined && req.body.game === undefined) {
+      res.json({
+        message:
+          "We can't update the game because you didn't give us any information with which we could update!!!",
+      });
+    }
+    if (req.body.description !== undefined) {
+      games[gameIndex].description = req.body.description;
+    }
+    if (req.body.game !== undefined) {
+      games[gameIndex].game = req.body.game;
+    }
+    res.json({ message: "The game has been updated!" });
+  }
+});
+
+router.delete("/delete-game/:id", function (req, res) {
+  let foundTheGame = games.findIndex((games) => games.id === req.params.id);
+  if (foundTheGame === -1) {
+    res.json({ message: "game not found, cannot delete" });
+  } else {
+    games.splice(foundTheGame, 1);
+    res.json({ payload: games });
+  }
+});
+
 module.exports = router;
